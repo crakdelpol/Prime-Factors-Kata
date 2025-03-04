6c3326796087a46993028ed4296e510a0833c384
Matteo Pipitone <matteo.pipitone@mdotm.ai>
2025-03-04 19:24:00 +0100

    second test pass

--- prima-factors-kata-java/src/test/java/it/matteopippi/PrimeFactorsUnitTest.java
+++ prima-factors-kata-java/src/test/java/it/matteopippi/PrimeFactorsUnitTest.java
@@ -20,12 +20,22 @@ public class PrimeFactorsUnitTest {
         assertTrue(factors.contains(2));
     }
 
+    @Test
+    void verifyPrimeFactorThreeIsThree() {
+        PrimeFactor primeFactor = new PrimeFactor();
+
+        List<Integer> factors = primeFactor.find(3);
+        assertEquals(1, factors.size());
+        assertTrue(factors.contains(3));
+    }
+
     private class PrimeFactor {
 
         public List<Integer> find(int i) {
-
+            if(i == 2){
                 return Arrays.asList(2);
-
+            }
+            return Arrays.asList(3);
         }
     }
 }
